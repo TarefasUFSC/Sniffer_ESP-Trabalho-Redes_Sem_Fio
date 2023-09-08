49d8ee2d776a99bb7fd12b4ea5c4d1318a23955f
CodeWracker <dev.rodrigofs@gmail.com>
2023-09-08 16:05:58 -0300

    tentei começar o codigo do esp AP, mas ele não esta recendendo nenhum dado

---
+++ ESP_B-Carrinho_AP/ESP_B-Carrinho_AP.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid = "ESP-B_Carrinho";
+
+// Defines a Port
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(9600);
+
+  // Configura o ESP8266 para operar apenas como um Access Point
+  WiFi.mode(WIFI_AP);
+  
+  // Cria um Access Point sem senha
+  WiFi.softAP(ssid);
+
+  // Inicia o servidor na porta 80
+  server.begin();
+
+  Serial.println("AP iniciado");
+  Serial.print("Endereço IP do AP: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void loop() {
+  // Verifica se há algum cliente tentando se conectar
+  WiFiClient client = server.available();
+  
+  if (client) {
+    Serial.println("Cliente conectado!");
+
+    // Aguarda até que o cliente envie algum dado
+    while (!client.available()) {
+      delay(1);
+    }
+
+    // Lê a mensagem enviada pelo cliente
+    String message = client.readStringUntil('\r');
+    Serial.print("Mensagem recebida: ");
+    Serial.println(message);
+
+    // Fecha a conexão com o cliente
+    client.stop();
+  }
+}
